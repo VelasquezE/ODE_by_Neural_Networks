fd956cfa9d1780a8e30e7a97ba60fe2d74bb178c
VelasquezE <132410679+VelasquezE@users.noreply.github.com>
2025-12-07 17:54:32 -0500

    Generalize for sending no matter which parameters

--- loss_func.py
+++ loss_func.py
@@ -1,6 +1,6 @@
 import torch
 
-criteria = torch.nn.MSELoss()
+mse = torch.nn.MSELoss()
 
 def get_grad(out: torch.Tensor, args: torch.Tensor):
     """
@@ -24,38 +24,53 @@ def get_grad(out: torch.Tensor, args: torch.Tensor):
             create_graph = True,
             only_inputs = True)
 
-def loss_constant(X, model):
-    out = model(X)
-    dx_2 = get_grad(out,X)[0]**2
-    return torch.mean(dx_2) + model(torch.tensor([1.0], requires_grad = True))**2
+def give_loss_harmonic_oscillator(points: torch.Tensor, model, params: dict):
+    """
+    Calculates the loss function for the harmonic oscillator.
+    
+    Parameters:
+        points : torch.Tensor
+            Collocation points
+        model:
+            Neural network.
+        params : dict
+            Dictionary containing the armonic oscillator parameters.
+            "w" (float), "x0" (float), "v0" (float).
+    Returns: 
+        Gives the loss function
+            diff_equation + initial_position + initial_velocity
+    """
+    device = points.device
+    dtype = points.dtype
 
-def loss_harmonic(T,model,w,m):
-    x = model(T)
-    dx_dt2 = get_grad(get_grad(x,T)[0],T)[0]
-    diff_equation = criteria(dx_dt2,- w * x)
-    grad_difeq = get_grad(diff_equation, T)[0]
+    x = model(points)
+    dx_dt = get_grad(x, points)[0]
+    dx_dt2 = get_grad(dx_dt, points)[0]
+    
+    diff_equation = mse(dx_dt2,- (params["w"] ** 2) * x)
 
-    x0 = torch.tensor([0.0], requires_grad = True)
-    y0 = model(x0)
-    dy0 = get_grad(y0,x0)[0]
+    t0 = torch.tensor([0.0], device = device, dtype = dtype, requires_grad = True)
 
-    inititial_conditions = criteria(y0,torch.zeros_like(y0)) + criteria(dy0,torch.ones_like(dy0))
-    return diff_equation + inititial_conditions #+ torch.max(grad_difeq**2) 
+    x0_predict= model(t0)
+    v0_predict = get_grad(x0_predict, t0)[0]
 
-def loss_harmonic_damped(T,model,params):
-    #parameters
-    m,b,k,=params
+    initial_position = mse(x0_predict, params["x0"] * torch.ones_like(x0_predict))  
+    initial_velocity = mse(v0_predict, params["v0"] * torch.ones_like(v0_predict))
+    return diff_equation + initial_position + initial_velocity
+
+def loss_harmonic_damped(points, model, params):
+    m, b, k = params
     
     #differential eq
-    x = model(T)
-    dx_dt = get_grad(x,T)[0]
-    dx_dt2 = get_grad(dx_dt,T)[0]
-    diff_equation = criteria(m*dx_dt2 + b*dx_dt, -k*x)
+    x = model(points)
+    dx_dt = get_grad(x, points)[0]
+    dx_dt2 = get_grad(dx_dt, points)[0]
+    diff_equation = mse(m * dx_dt2 + b * dx_dt, - k * x)
     
     #initial conditions
-    t0 = torch.tensor([0.0],requires_grad=True)
+    t0 = torch.tensor([0.0], requires_grad = True)
     x0 = model(t0)
-    dx_dt0 = get_grad(x0,t0)[0]
-    initial_conditions = criteria(x0,torch.zeros_like(x0)) + criteria(dx_dt0, torch.ones_like(dx_dt0))
+    dx_dt0 = get_grad(x0, t0)[0]
+    initial_conditions = mse(x0, torch.zeros_like(x0)) + mse(dx_dt0, torch.ones_like(dx_dt0))
 
     return diff_equation + initial_conditions
